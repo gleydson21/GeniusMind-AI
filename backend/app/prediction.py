2d3966ebc4d48014ce444b1109a892650a30bc75
Gleydson dias Ferreira <gleydson.dias@souunit.com.br>
2023-12-06 13:47:48 -0300

    commit -16
    configurar predict

--- backend/app/prediction.py
+++ backend/app/prediction.py
@@ -1,34 +1,29 @@
-# import pickle
+import tensorflow as tf
 
-# def load_model():
-#     # Caminho para o arquivo modelo.pkl gerado por sua IA
-#     model_path = '/data/modelo.pkl'
+def load_model():
+    # Caminho para o arquivo modelo.h5
+    model_path = './data/modelo.h5'
     
-#     try:
-#         # Carrega o modelo salvo no arquivo .pkl
-#         with open(model_path, 'rb') as file:
-#             model = pickle.load(file)
+    try:
+        # Carregar o modelo salvo no arquivo .h5
+        model = tf.keras.models.load_model(model_path)
             
-#         return model
-#     except Exception as e:
-#         print(f'Erro ao carregar o modelo: {str(e)}')
-#         return None
+        return model
+    except Exception as e:
+        print(f'Erro ao carregar o modelo: {str(e)}')
+        return None
 
-# def predict_student(input_data):
-#     model = load_model()
+def predict_student(input_data):
+    model = load_model()
     
-#     if model:
-#         # Realiza a previsão com base nos dados do formulário
-#         prediction = model.predict(input_data)
+    if model:
+        # Realiza a previsão com base nos dados do formulário
+        prediction = model.predict(input_data)
         
-#         # Verifica se o aluno foi aprovado ou reprovado
-#         if prediction == 1:
-#             return 'Aprovado'
-#         else:
-#             return 'Reprovado'
-#     else:
-#         return 'Não foi possível carregar o modelo denovo'
-    
-#     # A função load_model carrega o modelo treinado salvo no arquivo .pkl.
-#     # Em seguida, a função predict_student utiliza o modelo carregado para fazer a previsão com base nos dados do formulário.
-#     # No caso de uma previsão de valor 1, significa que o aluno foi aprovado, caso contrário, se for uma previsão de valor 0, o aluno foi reprovado.
+        # Verifica se o aluno foi aprovado ou reprovado
+        if prediction == 1:
+            return 'Aprovado'
+        else:
+            return 'Reprovado'
+    else:
+        return 'Não foi possível carregar o modelo'
