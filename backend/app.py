5596196c30b01b75a4e376b76c5022a8330d531c
Gleydson dias Ferreira <gleydson.dias@souunit.com.br>
2023-12-08 00:53:25 -0300

    commit-18
    ajustes

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, jsonify, render_template, request
 
 app = Flask(__name__)
 
@@ -6,10 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/inserirdados')
-def inserirdados():
-    # Lógica para renderizar a página de inserção de dados
-    return render_template('resultado')
+@app.route('/inserir_dados')
+def inserir_dados():
+    return render_template('inserirdados.html')
 
 
 
